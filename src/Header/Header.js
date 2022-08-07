f57e7a245da0be9baadb7fac72a0751aa66f6ca9
Zhixin chen <zhixin.chen72@gmail.com>
2022-08-07 01:02:13 -0400

    sda

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,13 +3,19 @@ import LogoImage from './logo.png';
 const Header = () => {
     return (
         <div className="header-container">
-                <img src={LogoImage} alt="Logo" />
-                <h3>EST. 2003</h3>
-                <div>组 约 永 龙 石 碑 公 司</div>
-                <div>New York Dragon Peace Enterprise Inc. (DPEINY)</div>
+               <div className='header_inner_left_container'>
+                 <img className='header_image' src = {LogoImage}/>
+                 <h5 className='left_subtitle'>EST. 2003</h5>
+               </div>
+               <div className='header_inner_right_container'>
+                  <h1 className='header_title'>组约永龙石碑公司</h1>
+                  <h5 className='right_subtitle'>New York Dragon Peace Enterprise Inc. (DPEINY)</h5>    
+                </div>
         </div>
     );
   };
+
+
 
 
   export default Header;
