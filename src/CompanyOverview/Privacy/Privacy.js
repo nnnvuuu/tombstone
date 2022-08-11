5f1df8ebc2cc9e562fd13fa7530a55ef6c35f9db
Zhixin chen <zhixin.chen72@gmail.com>
2022-08-10 20:46:26 -0400

    sdasd

--- src/CompanyOverview/Privacy/Privacy.js
+++ src/CompanyOverview/Privacy/Privacy.js
@@ -2,15 +2,17 @@ import Navbar from '../../NavBar/Navbarr';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
 import './Privacy.css';
+import { useTranslation } from "react-i18next";
 const Privacy = () => {
+    const { t } = useTranslation();
     return(
         <div>
             <Header/>
             <Navbar/>
             <div className='privacy_container'>
                 <div>
-                    <p>在永龍石碑的網站上，有許多永龍墓碑供訪客瀏覽。每個墓碑，都是一件藝術作品，每座墓碑背後，都有一個不平凡的故事。</p>
-                    <p>為了保護家人的隱私，永龍對在網上展示的石碑作品，已經做過模糊處理。但這會影響墓碑的完整觀瞻。永龍尊重家人的意願，如果家人不同意模糊處理，或是不同意墓碑圖片上網，請來電知會，永龍將按家人的意見處理。</p>
+                    <p>{t("Privacy.item1")}</p>
+                    <p>{t("Privacy.item2")}</p>
                 
                 </div>
             </div>
--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -2,7 +2,16 @@ import Navbar from '../NavBar/Navbarr';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import GoogleMap from '../Map/Map';
+import { Input } from 'antd';
+import Button from 'react-bootstrap/Button';
+import { TelephoneFill, GeoAltFill, EnvelopeFill, Clock } from 'react-bootstrap-icons';
+import WeChatImg from '../HomePgae/Information/wechat.svg';
+
+
 import './ContactUs.css';
+const { TextArea } = Input;
+
+
 const ContactUs = () => {
     return(
         <div>
@@ -11,15 +20,54 @@ const ContactUs = () => {
             <div className = "ContactUs_container">
                 <div  className = "ContactUs_inner_container">
                    <div className = "ContactUs_inner_left_container">
-                       <h2>获取报价</h2>
-                       <textarea row="4" cols="2">
-                         你好， 这是在 text area 里的文本
-                       </textarea>
+                       <h1 className='left_title'>給我們留言</h1>
+                       <div className='ContactUs_inner_left_flexFirrstRow_container'>
+                       <TextArea className ="name_field"  placeholder={"Name"}/>
+                       <TextArea className ="email_field"  placeholder={"Email"}/>
+                       </div>
+                       <div className='ContactUs_inner_left_flexSecondRow_container'>
+                       <TextArea rows={2} cols={10} className ="phone_field"  placeholder={"Phone"}/>
+                       </div>
+                       <div className='ContactUs_inner_left_flexThirdRow_container'>
+                       <TextArea rows={2} cols={10} className ="message_field"  placeholder={"Message"}/>
+                       </div>
+                       <Button variant="primary" className='submit_button'>Send</Button>{' '}
                    </div>
+                   
                    <div className = "ContactUs_inner_right_container">
-                       <h1>聯繫我們</h1>
-                   </div>
+                       <h1 className='right_title'>聯繫我們</h1>
+                        <div className = "inner_wrapper" style={{marginTop:"5px"}}>
+                             <div className='right_firstRow'>
+                                <TelephoneFill className='telephone'/>
+                                <h5 className='h5'>Phone Number</h5>
+                            </div>
+                            <a href="tel:+1-718-492-8959" className='remove_a_tag_css' ><p className='p'>718-492-8959</p></a>
+                            <a href="tel:+1-718-492-8959" className='remove_a_tag_css' ><p className='p'>718-492-5111</p></a>
+                      
+                             <div className='right_firstRow'>
+                                <GeoAltFill className='address'/>
+                                <h5 className='h5'>Location</h5>
+                            </div>
+                                <p className='p'>5008 4th Ave</p>
+                                <p className='p'>Brooklyn NY, 11220</p>
+                         
+                
+                            <div className='right_firstRow'>
+                                <EnvelopeFill className='email'/>
+                                <h5 className='h5'>Email</h5>
+                            </div>
+                                <p className='p'>dpeistone@gmail.com</p>
+                                <p className='p'>dpetrade@hotmail.com</p>
+                   
+                            <div className='right_firstRow'>
+                                <img src={WeChatImg} className="wechat_img"/>
+                                <h5 className='h5'>WeChat</h5>
+                            </div>
+                                <p className='p' >@dpeiny</p>
+                        </div>
+                   </div>     
                 </div>
+                
             </div>
             <GoogleMap />
             <Footer/>
