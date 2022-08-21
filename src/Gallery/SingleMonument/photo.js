4e3c55d73f648707c3e215a93d5fff7d997cad9e
Zhixin chen <zhixin.chen72@gmail.com>
2022-08-21 18:38:37 -0400

    sdad

--- src/Gallery/SingleMonument/photo.js
+++ src/Gallery/SingleMonument/photo.js
@@ -27,102 +27,102 @@ export const photos = [
     {
       src: img1,
       width: 5,
-      height: 5,
+      height: 4,
 
     },
     {
         src: img2,
       width: 5,
-      height: 5,
+      height: 4,
     },
     {
         src: img3,
         width: 5,
-        height: 5,
+        height: 4,
   
     },
     {
         src: img4,
         width: 5,
-        height: 5,
+        height: 4,
   
     },
      {
         src: img5,
         width: 5,
-        height: 5,
+        height: 4,
   
     },
     {
         src: img6,
         width: 5,
-        height: 5,
+        height: 4,
   
     },
     {
         src: img7,
         width: 5,
-        height: 5,
+        height: 4,
   
     },  
     {
         src: img8,
         width: 5,
-        height: 5,
+        height: 4,
   
     },  
     {
         src: img9,
         width: 5,
-        height: 5,
+        height: 4,
   
     },  
     {
         src: img10,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img11,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img12,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img13,
         width: 5,
-        height: 5,
+        height: 4,
   
     },
     {
         src: img14,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img15,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img16,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     {
         src: img17,
         width: 5,
-        height: 5,
+        height: 4,
   
     }, 
     
